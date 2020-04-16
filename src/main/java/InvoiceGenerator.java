be6384ea4faaab4fb48dcccb67f528af81caaa07
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-16 15:31:11 +0530

    Added: Calculate Fare And Aggregate of Multiple Rides

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -3,11 +3,19 @@ public class InvoiceGenerator {
     public static final double MIN_COST_PER_KMS = 10 ;
     public static final int COST_PER_TIME = 1 ;
     public static final int MIN_FARE=5;
+    double totalFare=0;
 
     public double calculateFare(double distance, int time) {
         double totalFare= distance*MIN_COST_PER_KMS + time*COST_PER_TIME;
         if ( totalFare < MIN_FARE)
             return MIN_FARE;
+        return totalFare;
+    }
+
+    public double calculateFare(Ride[] rides) {
+        for (Ride ride :rides ) {
+            totalFare+=this.calculateFare(ride.distance, ride.time);
+        }
         return totalFare;
     }
 }
